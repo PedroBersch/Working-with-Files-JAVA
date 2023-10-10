85b1d99e4798ae99128e2221593c9d07c9e7a496
Pedro Bersch <pedroleottebersch@gmail.com>
2023-10-10 20:00:35 -0300

    Buffer and File Reader - bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,38 @@
 package application;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        File file = new File("..\\files\\in.txt");
-        Scanner sc = null;
+        String path = "..\\files\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            System.out.println(line);
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
-        }
-        catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            if (sc != null) {
-                sc.close();
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    fr.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
             }
         }
     }
